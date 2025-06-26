0a750d83de03d3a297432beba6ae8b107296bc8c
Miguel Ángel Rodríguez <m.angel.rdzl15@gmail.com>
2025-06-25 18:09:50 -0600

    Leer credenciales Firebase desde variable de entorno

--- config/firebase.js
+++ config/firebase.js
@@ -1,16 +1,12 @@
-// Importa el SDK de administración de Firebase (firebase-admin), necesario para acceder a Firestore y otros servicios desde el backend
 const admin = require('firebase-admin');
 
-// Carga el archivo JSON con las credenciales del servicio (clave privada generada desde Firebase)
-const serviceAccount = require('../serviceAccountKey.json');
+// Leer el contenido JSON desde la variable de entorno FIREBASE_CONFIG
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
-// Inicializa la aplicación de Firebase con las credenciales de servicio
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount) // Se usa el certificado para autenticar como una cuenta de servicio
+  credential: admin.credential.cert(serviceAccount)
 });
 
-// Obtiene una instancia de la base de datos Firestore
 const db = admin.firestore();
 
-// Exporta la instancia de Firestore para poder utilizarla en otros módulos del proyecto
 module.exports = db;
